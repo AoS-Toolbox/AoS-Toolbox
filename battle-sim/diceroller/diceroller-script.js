1169ad5991c095af5337400a28f8a6d82df13fda
decadentpig <jordandevisser@gmail.com>
2022-01-02 00:59:38 +1000

    Dice roller basic functionality complete

--- battle-sim/diceroller/diceroller-script.js
+++ battle-sim/diceroller/diceroller-script.js
@@ -3,6 +3,7 @@ let diceStage;
 
 let numDiceInTray = 0;
 let diceTrayObjects = [];
+let rolledDiceObjects = [];
 let numDiceText;
 
 let rollResults = {
@@ -13,6 +14,7 @@ let rollResults = {
   5: null,
   6: null
 }
+let readOuts = [];
 
 // INITIALISE STAGE, BUTTONS & CONTAINERS
 function dicerollerInit() {
@@ -26,7 +28,7 @@ function dicerollerInit() {
   numDiceText = new createjs.Text(`(${numDiceInTray})`, '18px Arial', "white");
   numDiceText.textBaseline = "alphabetic";
   numDiceText.x = 70;
-  numDiceText.y = 155;
+  numDiceText.y = 190;
   
   diceStage.addChild(numDiceText);
   diceStage.update();
@@ -41,14 +43,14 @@ function initDiceButtons() {
   const addDiceButtons = [add1D6Button, add2D6Button, add5D6Button, add10D6Button];
   
   for (let i = 0; i < addDiceButtons.length; i++) {
-    addDiceButtons[i].graphics.s("black").f("white").drawRect(0, 0, 65, 30);
+    addDiceButtons[i].graphics.s("black").f("white").drawRect(0, 0, 60, 30);
     addDiceButtons[i].y = 10;
   }
   
   add1D6Button.x = 5;
-  add2D6Button.x = 80;
-  add5D6Button.x = 155;
-  add10D6Button.x = 230;
+  add2D6Button.x = 75;
+  add5D6Button.x = 145;
+  add10D6Button.x = 215;
   
   add1D6Button.numDice = 1;
   add2D6Button.numDice = 2;
@@ -83,7 +85,7 @@ function initDiceButtons() {
     text.x = addDiceButtons[i].x + 14;
     text.y = addDiceButtons[i].y + 7;
     
-    if (i === 3) text.x = addDiceButtons[i].x + 12;
+    if (i === 3) text.x = addDiceButtons[i].x + 10;
     
     diceStage.addChild(text);
   }
@@ -91,28 +93,27 @@ function initDiceButtons() {
 
 function initClearButton() {
   let clearButton = new createjs.Shape();
-  clearButton.graphics.s("black").f("white").drawRect(5, 155, 60, 20);
+  clearButton.graphics.s("black").f("white").drawRect(5, 175, 60, 20);
   
   clearButton.on("click", clearTray);
+  clearButton.on("click", clearResults);
   
   let clearText = new createjs.Text("CLEAR", "15px Arial", "black");
   clearText.x = 9;
-  clearText.y = 158;
+  clearText.y = 178;
   
   diceStage.addChild(clearButton, clearText);
 }
 
 function initRollButton() {
   let rollButton = new createjs.Shape();
-  rollButton.graphics.s("black").f("white").drawRect(0, 0, 60, 20);
-  rollButton.x = 235;
-  rollButton.y = 155;
+  rollButton.graphics.s("black").f("white").drawRect(215, 175, 60, 20);
   
   rollButton.on("click", rollDice);
   
   let rollText = new createjs.Text("ROLL", "15px Arial", "black");
-  rollText.x = 246;
-  rollText.y = 158;
+  rollText.x = 225;
+  rollText.y = 178;
   
   diceStage.addChild(rollButton, rollText);
 }
@@ -151,9 +152,22 @@ function buildResultsContainers() {
     delText.x = 14;
     delText.y = posY + 5;
     
-    diceStage.addChild(del, delText);
+    let container = new createjs.Shape();
+    container.graphics.f("#6e878a").drawRect(60, posY-10, 210, 40);
+    
+    diceStage.addChild(container, del, delText);
   }
   
+  let reRollButton = new createjs.Shape();
+  reRollButton.graphics.s("black").f("white").drawRect(200, 555, 70, 30);
+  
+  reRollButton.on("click", reRoll);
+  
+  let reRollText = new createjs.Text("RE-ROLL", "15px Arial", "black");
+  reRollText.x = 203;
+  reRollText.y = 563;
+  
+  diceStage.addChild(reRollButton, reRollText);
   diceStage.update();
 }
 
@@ -164,7 +178,7 @@ function addDice(event) {
   if (numDiceInTray > 60) numDiceInTray = 60;
   redrawDiceTray();
   updateDiceTrayCounter();
-};
+}
 
 function redrawDiceTray() {
   for (let i = 0; i < diceTrayObjects.length; i++) {
@@ -181,20 +195,22 @@ function redrawDiceTray() {
   }
   
   for (let i = 0; i < diceTrayObjects.length; i++) {
-    if (i < 12) {
+    if (i < 10) {
       diceTrayObjects[i].y = 50;
-    } else if (i < 24) {
+    } else if (i < 20) {
       diceTrayObjects[i].y = 70;
-    } else if (i < 36) {
+    } else if (i < 30) {
       diceTrayObjects[i].y = 90;
-    } else if (i < 48) {
+    } else if (i < 40) {
       diceTrayObjects[i].y = 110;
-    } else if (i < 60) {
+    } else if (i < 50) {
       diceTrayObjects[i].y = 130;
+    } else if (i < 60) {
+      diceTrayObjects[i].y = 150;
     }
     
-    if (i === 0 || i === 12 || i === 24 || i === 36 || i === 48 || i === 60) {
-      diceTrayObjects[i].x = 5;
+    if (i === 0 || i === 10 || i === 20 || i === 30 || i === 40 || i === 50) {
+      diceTrayObjects[i].x = 20;
     } else {
       diceTrayObjects[i].x = diceTrayObjects[i - 1].x + 25;
     }
@@ -209,14 +225,22 @@ function clearTray() {
     numDiceInTray = 0;
     redrawDiceTray();
     updateDiceTrayCounter();
-  }
+}
 
 function updateDiceTrayCounter() {
   numDiceText.text = `(${numDiceInTray})`;
   diceStage.update();
 }
 
+function clearResults() {
+  for (let i = 0; i < readOuts.length; i++) { diceStage.removeChild(readOuts[i]); }
+  readOuts = [];
+  diceStage.update();
+}
+
 function rollDice() {
+  clearResults();
+  
   let results = [];
   
   for (let i = 0; i < numDiceInTray; i++) {
@@ -234,15 +258,63 @@ function rollDice() {
 }
 
 function reRoll() {
-  
+  let count = 0;
+  for (let i = 1; i <= 6; i++) {
+    count += rollResults[i];
+  }
+  numDiceInTray = count;
+  rollDice();
 }
 
 function showDiceResults() {
-  
+  // Loop over each D6 result - 1 to 6
+  for (let i = 1; i <= 6; i++) {
+    let readOut = new createjs.Text(`(${rollResults[i]})`, "18px Arial", "white");
+    readOut.diceValue = i;
+    readOut.x = 70;
+    
+    switch (i) {
+      case 1:
+        readOut.y = 260;
+        break;
+      case 2:
+        readOut.y = 310;
+        break;
+      case 3:
+        readOut.y = 360;
+        break;
+      case 4:
+        readOut.y = 410;
+        break;
+      case 5:
+        readOut.y = 460;
+        break;
+      case 6:
+        readOut.y = 510;
+        break;
+    }
+    
+    if (rollResults[i] > 0) {
+      readOuts.push(readOut);
+      diceStage.addChild(readOut);
+    }
+  }
+  diceStage.update();
 }
 
 function deleteRolledDice(event) {
-  // use event.target.diceValue to determine which row to delete
+  console.log("Entering deleteRolledDice()");
+  let deleteNum = event.target.diceValue;
+  rollResults[deleteNum] = 0;
+  for (let i = 0; i < readOuts.length; i++) {
+    console.log("Loop ", i+1);
+    if (readOuts[i].diceValue === deleteNum) {
+      console.log("Deleting a ", readOuts[i].diceValue);
+      diceStage.removeChild(readOuts[i]);
+    }
+  }
+  readOuts = readOuts.filter(element => element.diceValue !== deleteNum);
+  diceStage.update();
 }
 
 // DICEROLLING LOGIC
